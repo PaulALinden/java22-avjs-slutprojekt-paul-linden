90858e9e6de61f28d367f90c8330fd4803dd9616
Paul Alexander Lindén <142324909+PaulALinden@users.noreply.github.com>
2024-02-01 09:36:26 +0100

    Update firebaseApp.js

--- src/modules/firebaseApp.js
+++ src/modules/firebaseApp.js
@@ -1,15 +1,6 @@
 import { initializeApp } from "firebase/app";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAZz0uxCjCVsimv9LC1FJXr0dv4tC7QTZU",
-  authDomain: "react-shop-45c8b.firebaseapp.com",
-  databaseURL: "https://react-shop-45c8b-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "react-shop-45c8b",
-  storageBucket: "react-shop-45c8b.appspot.com",
-  messagingSenderId: "388969009761",
-  appId: "1:388969009761:web:1439c7887e1c10d1329020",
-  measurementId: "G-MQKSVHV6VN"
-};
+const firebaseConfig = {};
 
 const app = initializeApp(firebaseConfig);
 
